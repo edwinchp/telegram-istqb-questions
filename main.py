ffd6c5579f3f71919d8daf22fa4a3a4e07859acc
Edwin Chi <chipineiro@gmail.com>
2023-04-24 18:43:17 -0600

    Get token from json

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import telegram
+import json
+import random
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Read the token and chat id from the JSON file
+with open('setup.json') as f:
+    bot = json.load(f)["token"]
+
+with open('setup.json') as f:
+    chat_id = json.load(f)["chat_id"]
+
+# Read the questions and answers from the JSON file
+with open('questions.json') as f:
+    questions = json.load(f)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while True:
+    # Choose a random question from the list
+    question = random.choice(questions)
 
+    # Re-order the options randomly
+    random.shuffle(question['options'])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Send the poll to the Telegram chat
+    poll = bot.send_poll(chat_id=chat_id, question=question['question'], options=question['options'], correct_option_id=question['answer'])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Wait for 60 seconds before sending the next poll
+    time.sleep(60)
