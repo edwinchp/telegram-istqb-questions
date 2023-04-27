d7ecfebd4fb802c0a542745b6772b138566f585c
Edwin Chi <chipineiro@gmail.com>
2023-04-26 18:37:57 -0600

    Save work

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import json
 import random
 import time
 
+from telegram.ext import Updater
+
 # Read the token and chat id from the JSON file
 with open('setup.json') as f:
-    bot = json.load(f)["token"]
+    token = json.load(f)["token"]
 
 with open('setup.json') as f:
     chat_id = json.load(f)["chat_id"]
@@ -14,16 +16,24 @@ with open('setup.json') as f:
 with open('questions.json') as f:
     questions = json.load(f)
 
+bot = telegram.Bot(token=token)
 
-while True:
-    # Choose a random question from the list
-    question = random.choice(questions)
+#while True:
+# Choose a random question from the list
+question = random.choice(questions)
 
-    # Re-order the options randomly
-    random.shuffle(question['options'])
+# Re-order the options randomly
+random.shuffle(question['options'])
 
-    # Send the poll to the Telegram chat
-    poll = bot.send_poll(chat_id=chat_id, question=question['question'], options=question['options'], correct_option_id=question['answer'])
+# Send the poll to the Telegram chat
+#poll = bot.send_poll(chat_id=chat_id, question=question['question'], options=question['options'], correct_option_id=question['answer'])
 
-    # Wait for 60 seconds before sending the next poll
-    time.sleep(60)
+bot.send_message(chat_id=chat_id, text="Hello from python")
+
+
+print(token)
+print(chat_id)
+print("Success")
+
+# Wait for 60 seconds before sending the next poll
+time.sleep(60)
