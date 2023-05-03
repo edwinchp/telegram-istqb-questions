55fb5001253a8f269033f240e9218187196716fb
Edwin Chi <chipineiro@gmail.com>
2023-05-02 18:58:31 -0600

    Add exception when status code is not 200

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ response = requests.post(url, data=payload)
 if response.status_code == 200:
     print('Poll sent successfully.')
 else:
-    print(f'Error sending poll: {response.status_code} - {response.text}')
+    raise Exception(f'Error sending poll: {response.status_code} - {response.text}')
