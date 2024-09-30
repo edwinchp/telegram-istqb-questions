9c12e6e429c5b896a19b38b1d800737788ea382b
chipi <chipineiro@gmail.com>
2024-09-29 23:33:42 -0600

    Create tests

---
+++ tests/test_question.py
@@ -0,0 +1,15 @@
+from factories.data_factory import DataFactory
+from factories.question_factory import QuestionFactory
+import pytest
+
+
+@pytest.fixture
+def sample_question():
+    data = DataFactory.get_random_data()
+    question = QuestionFactory.create_question(data)
+    return question
+
+class TestQuestion:
+    def test_question_has_id(self, sample_question):
+        assert sample_question is not None
+
